2761f8696b6e5d24d588292811e06b9f1a438057
Julia Delis <juliadelis2@hotmail.com>
2024-01-29 15:16:54 -0300

    chore: adding hero section

--- my-app/components/Button.tsx
+++ my-app/components/Button.tsx
@@ -5,15 +5,14 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: string;
 };
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
   return (
     <button
       className={`flexCenter gap-3 rounded-full border ${variant}`}
-      type={type}
-    >
+      type={type}>
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className=" flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
@@ -15,8 +15,7 @@ const Navbar = () => {
           <Link
             href={link.href}
             key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
             {link.label}
           </Link>
         ))}
